337a785280dc550f7ac4dd88abfb9318542d3d15
GithubArnavSharma <77365987+GithubArnavSharma@users.noreply.github.com>
2023-07-25 13:30:35 -0700

    Update spanish_oral.py

--- spanish_oral.py
+++ spanish_oral.py
@@ -6,6 +6,8 @@ import pyttsx3
 import cv2
 import random
 from tkinter import *
+from tkinter.ttk import *
+from ttkthemes import ThemedTk
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 from googletrans import Translator
 from threading import *
@@ -29,12 +31,13 @@ def speak(spanish_text):
 recording_text = ""
 questions = []
 do_record = True
+recorded_before = False
 def record_audio(filename):
     chunk = 1024
     FORMAT = pyaudio.paInt16
     channels = 1
     sample_rate = 44100
-    record_seconds = 20
+    record_seconds = 120
     p = pyaudio.PyAudio()
 
     stream = p.open(format=FORMAT,
@@ -77,14 +80,18 @@ def audio_to_spanish(filename):
         try:
             result = r.recognize_google(audio, language="es-ES")
         except:
-            result = "N/A: Please Speak Louder!"
+            result = "\nN/A: Please Speak Louder!"
         
     os.remove(filename)
 
     return result
 
 
-app = Tk()
+#app = Tk()
+app = ThemedTk(theme="yaru")
+height = 470
+width = 500
+app.geometry(f"{width}x{height}")
 
 def exit_window():
     global rule_window
@@ -99,31 +106,45 @@ def question_right():
     questions.remove(current_question)
     exit_window()
 
+def newline_text(text):
+    add_nl = 14
+    
+    words = text.split()
+    for i in range(add_nl, len(words), add_nl):
+        words[i] += "\n"
+    text_new = " ".join(words)
+    
+    return text_new
+
 def display_results(spanish_text, english_text):
     global rule_window
     rule_window = Toplevel(app)
     
     rule_window.title("Results")
-    rule_window.geometry("750x150")
+    #rule_window.geometry("750x150")
 
-    instruct_lbl = Label(rule_window, text="Based on the Spanish and English translation of what you said, did you say it correctly?", font=("Arial", 11))
-    instruct_lbl.pack()
+    instruct_lbl = Label(rule_window, text="Based on the Spanish and English translation of what you said,\ndid you say it correctly?", 
+                         font=('Microsoft YaHei', 12), background="#F0F0F0")
+    instruct_lbl.pack(padx=10)
     
-    spanish = Label(rule_window, text=spanish_text, foreground="black", font=("Arial", 11))
-    spanish.pack(pady=10, anchor="w", side=TOP)
-    english = Label(rule_window, text=english_text, foreground="black", font=("Arial", 11))
-    english.pack(pady=10, anchor="w", side=TOP)
+    spanish = Label(rule_window, text=spanish_text, foreground="black", font=('Microsoft YaHei', 11), background="#F0F0F0")
+    spanish.pack(pady=10, padx=5, anchor="w", side=TOP)
+    english = Label(rule_window, text=english_text, foreground="black", font=('Microsoft YaHei', 11), background="#F0F0F0")
+    english.pack(pady=10, padx=5, anchor="w", side=TOP)
 
-    correct = Button(rule_window, text="Correct", command=question_right, font=("Arial", 11))
-    correct.pack(padx=150, side=LEFT)
+    correct = Button(rule_window, text="Correct", command=question_right)
+    correct.pack(padx=50, side=LEFT, pady=10)
 
-    wrong = Button(rule_window, text="Wrong", command=exit_window, font=("Arial", 11))
-    wrong.pack(padx=150, side=RIGHT)
+    wrong = Button(rule_window, text="Wrong", command=exit_window)
+    wrong.pack(padx=50, side=RIGHT, pady=10)
     
 
 def spanish_question():
 
     if len(questions) == 0: return None
+
+    global recorded_before
+    recorded_before = True
 
     filename = 'temp.wav'
 
@@ -142,8 +163,8 @@ def spanish_question():
     spanish_text = audio_to_spanish(filename)
     english_text = spanish_to_english(spanish_text)
 
-    display_results(f"What you said in Spanish: {spanish_text}",
-                    f"What it means in English: {english_text}")
+    display_results(f'What you said in Spanish:\n"{newline_text(spanish_text)}"',
+                    f'What it means in English:\n"{newline_text(english_text)}"')
 
     global do_record
     do_record = True
@@ -153,31 +174,41 @@ def thread_question():
     t1.start()
 
 def record_off():
-    if len(questions) == 0: return None
+    global recorded_before
+    if len(questions) == 0 or not recorded_before: return None
     
     global do_record
     do_record = False
+    recorded_before = False
 
 def record_text():
     global text
     text = "Recording..."
 
 
-q_instruct = Label(app, text="Write a question and click 'Add Question' to add the question!", font=("Arial", 11), foreground="black")
-inputtxt = Text(app, height = 10, width = 40, font=("Arial", 11))
+q_instruct = Label(app, text="Write a question and click 'Add Question' to add the question!", font=('Microsoft YaHei', 12), foreground="black", background="#F0F0F0")
+inputtxt = Text(app, height = 15, width = 45, font=('Microsoft YaHei', 11))
 
 q_instruct.pack(pady=10)
 inputtxt.pack(pady=5)
 
 def printInput():
-    lbl = Label(app, text = "", font=("Arial", 11))
+    lbl = Label(app, text = "")
 
     lbl.pack()
     inp = inputtxt.get(1.0, "end-1c")
 
     global questions
     questions.append(inp)
-    lbl.config(text = f"{len(questions)}. {inp}")
+
+    global height
+    height += 30
+    app.geometry(f"{width}x{height}")
+    
+    lbl.config(text = f"{len(questions)}. {inp}", font=('Microsoft YaHei', 11), background="#F0F0F0")
+
+    spaceLbl = Label(app, text= "", font=('Microsoft YaHei', 4), background="#F0F0F0")
+    spaceLbl.pack()
 
 def restart_questions():
     global questions, questions_text
@@ -193,7 +224,7 @@ def startCamera():
 
     global answer_q, stop_record, vid, label_widget, questions_text
 
-    app = Tk()
+    app = ThemedTk(theme="yaru")
 
     vid = cv2.VideoCapture(0)
 
@@ -204,22 +235,29 @@ def startCamera():
     
     app.bind('<Escape>', lambda e: app.quit())
 
-    instruct_txt = "Press 'Begin Question' when you're ready to be asked a question.\nWhen the 'Recording...' text comes up, begin answering it.\nWhen you are done"
-    instruct_txt += " answering the question, click 'Finish Question'.\nWhen you have finished all of the questions, you may restart by clicking 'Restart'."
-    instruct_lbl = Label(app, text=instruct_txt, font=("Arial", 11), foreground="black")
+    welcome_txt = "  Practice your Oral Skills!"
+    welcome_lbl = Label(app, text=welcome_txt, foreground="black", font=('Microsoft YaHei', 14), background="#F0F0F0")
+    welcome_lbl.pack()
+
+    pad_lbl = Label(app, text="", foreground="black", font=('Microsoft YaHei', 6), background="#F0F0F0")
+    pad_lbl.pack()
+    
+    instruct_txt = "-  Press 'Begin Question' when you're ready to be asked a question.\n-  When the 'Recording...' text comes up, begin answering it.\n-  When you are done"
+    instruct_txt += " answering the question, click 'Finish Question'.\n-  When you have finished all of the questions, you may restart by clicking 'Restart'."
+    instruct_lbl = Label(app, text=instruct_txt, foreground="black", font=('Microsoft YaHei', 11), background="#F0F0F0")
 
     instruct_lbl.pack()
     
     label_widget = Label(app)
     label_widget.pack(pady=10)
 
-    answer_q = Button(app, text="Begin Question", command=thread_question, font=("Arial", 10))
+    answer_q = Button(app, text="Begin Question", command=thread_question)
     answer_q.pack(pady=5, side=LEFT, padx=25)
 
-    stop_record = Button(app, text="Finished Question", command=record_off, font=("Arial", 10))
+    stop_record = Button(app, text="Finished Question", command=record_off)
     stop_record.pack(pady=5, side=LEFT, padx=5)
 
-    restart = Button(app, text="Restart", command=restart_questions, font=("Arial", 10))
+    restart = Button(app, text="Restart", command=restart_questions)
     restart.pack(pady=5, side=RIGHT, padx=65)
 
     global total_questions
@@ -232,10 +270,10 @@ def startCamera():
     app.mainloop()
 
     
-addBtn = Button(app, text = "Add Question", command = printInput, font=("Arial", 10))
+addBtn = Button(app, text = "Add Question", command = printInput)
 addBtn.pack(pady=5)
 
-startBtn = Button(app, text = "Finish", command = startCamera, font=("Arial", 10))
+startBtn = Button(app, text = "Finish", command = startCamera)
 startBtn.pack(pady=5)
 
 #Credit:
@@ -248,12 +286,12 @@ def open_camera():
     captured_image = Image.fromarray(opencv_image)
 
     global recording_text, questions_text
-    font = ImageFont.truetype("arial.ttf", 25)
+    font = ImageFont.truetype("Microsoft Yahei.ttf", 20)
     
     ImageDraw.Draw(
         captured_image
     ).text(
-        (0, 0), 
+        (5, 0), 
         recording_text,
         (0, 0, 0),
         font=font
@@ -262,7 +300,7 @@ def open_camera():
     ImageDraw.Draw(
         captured_image
     ).text(
-        (320, 0),
+        (360, 0),
         questions_text,
         (0, 0, 0),
         font=font
